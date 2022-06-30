df09bb38a9ff3d9714339ae2f60893cb7d6fa205
DikshaMahajan-coder <81462023+DikshaMahajan-coder@users.noreply.github.com>
2022-06-30 16:16:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here//
+const firebaseConfig = {
+    apiKey: "AIzaSyAWaFtzWppRcydAnMpMNo0Ck1tArwKwwhQ",
+    authDomain: "project-71-44a03.firebaseapp.com",
+    projectId: "project-71-44a03",
+    storageBucket: "project-71-44a03.appspot.com",
+    messagingSenderId: "748427201702",
+    appId: "1:748427201702:web:7e2de3eb787a0d07353867"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
